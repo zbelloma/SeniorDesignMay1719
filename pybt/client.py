fc248f241ec23b34bb1da095b62b747cf66690b4
Michael Rupert <mbrupert@istate.edu>
2017-02-10 19:01:05 -0600

    replaced with bluez

--- pybt/client.py
+++ pybt/client.py
@@ -1,17 +1,17 @@
 """
-A simple Python script to send messages to a sever over Bluetooth using
-Python sockets (with Python 3.3 or above).
+A simple Python script to send messages to a sever over Bluetooth
+using PyBluez (with Python 2).
 """
 
-import socket
+import bluetooth
 
 serverMACAddress = 'B8:27:EB:76:A0:C9'
 port = 3
-s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.connect((serverMACAddress,port))
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.connect((serverMACAddress, port))
 while 1:
-    text = input()
+    text = raw_input() # Note change to the old (Python 2) raw_input
     if text == "quit":
-        break
-    s.send(bytes(text, 'UTF-8'))
-s.close()
+    break
+    s.send(text)
+sock.close()
--- pybt/server.py
+++ pybt/server.py
@@ -1,24 +1,24 @@
 """
 A simple Python script to receive messages from a client over
-Bluetooth using Python sockets (with Python 3.3 or above).
+Bluetooth using PyBluez (with Python 2).
 """
 
-import socket
+import bluetooth
 
 hostMACAddress = 'B8:27:EB:76:A0:C9' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
-port = 3 # 3 is an arbitrary choice. However, it must match the port used by the client.
+port = 3
 backlog = 1
 size = 1024
-s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.bind((hostMACAddress,port))
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.bind((hostMACAddress, port))
 s.listen(backlog)
 try:
-    client, address = s.accept()
+    client, clientInfo = s.accept()
     while 1:
         data = client.recv(size)
         if data:
             print(data)
-            client.send(data)
+            client.send(data) # Echo back to client
 except:
     print("Closing socket")
     client.close()

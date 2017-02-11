e322903d3d542d4156ef631edfe48d6b08066514
Michael Rupert <mbrupert@istate.edu>
2017-02-10 18:54:22 -0600

    fixed pybt stuff

--- pybt/server.py
+++ pybt/server.py
@@ -1,17 +1,25 @@
 """
-A simple Python script to send messages to a sever over Bluetooth using
-Python sockets (with Python 3.3 or above).
+A simple Python script to receive messages from a client over
+Bluetooth using Python sockets (with Python 3.3 or above).
 """
 
 import socket
 
-serverMACAddress = 'B8:27:EB:76:A0:C9'
-port = 3
+hostMACAddress = 'B8:27:EB:76:A0:C9' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+port = 3 # 3 is an arbitrary choice. However, it must match the port used by the client.
+backlog = 1
+size = 1024
 s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.connect((serverMACAddress,port))
-while 1:
-    text = input()
-    if text == "quit":
-        break
-    s.send(bytes(text, 'UTF-8'))
-s.close()
+s.bind((hostMACAddress,port))
+s.listen(backlog)
+try:
+    client, address = s.accept()
+    while 1:
+        data = client.recv(size)
+        if data:
+            print(data)
+            client.send(data)
+except:
+    print("Closing socket")
+    client.close()
+    s.close()

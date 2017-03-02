1e65268ddcf206a5cd9742e908614f8f55bd808c
Michael Rupert <mbrupert@istate.edu>
2017-03-02 12:29:47 -0600

    Fixed ignored files, updated chemicalviewer

--- chemicalviewer/src/App.js
+++ chemicalviewer/src/App.js
@@ -30,48 +30,53 @@ class App extends Component {
 
   render() {
     var db = this.state.db;
+
     function adjustData(data){
-      var newData = new Array(977);
+      var newData = new Array(1101);
       var index = 0;
-      for(var j = 30; j < data.length; j += 3){
-        if(j<30){
-          newData[index] = null;
+      for(var j = 0; j < newData.length; j++){
+        if(j<190){
+          newData[j] = null;
+        } else if(index < data.length){
+          newData[j] = data[index];
           index++;
         } else {
-          newData[index] = data[j];
-          index++;
+          newData[j] = null;
         }
       }
+
       newData.unshift("Scan");
       return newData;
     }
     const listItems = Object.keys(this.state.db).map((v, index) => {
-      var data = db[v]['pixels'].slice(40, 3000)
-      var newData = adjustData(data);
-      var chartData = {
-        columns: [newData]
-      }
-      var chartAxis = {
-        x: {
-          label: "Wavelength (nm)",
-          min: 190,
-          max: 1100,
-        },
-        y: {
-          label: "Intensity",
+      var data = db[v]['pixels']
+      if(data != null){
+        var newData = adjustData(data);
+        var chartData = {
+          columns: [newData]
         }
+        var chartAxis = {
+          x: {
+            label: "Wavelength (nm)",
+            min: 190,
+            max: 1100,
+          },
+          y: {
+            label: "Intensity",
+          }
+        }
+        var date = new Date(db[v]['time']);
+        return <tr key={index}>
+                <td>{dateFormat(date, "m/dd/yy h:MM TT") || 'N/A'}</td>
+                <td>
+                  <Collapse accordian={true}>
+                    <Panel header="Chart">
+                      <C3Chart data={chartData} type={'spline'} point={{show: false}} names={{data1: 'Scan'}} axis={chartAxis} />
+                    </Panel>
+                  </Collapse>
+                </td>
+               </tr>
       }
-      var date = new Date(db[v]['time']);
-      return <tr key={index}>
-              <td>{dateFormat(date, "m/dd/yy h:MM TT") || 'N/A'}</td>
-              <td>
-                <Collapse accordian={true}>
-                  <Panel header="Chart">
-                    <C3Chart data={chartData} type={'spline'} point={{show: false}} names={{data1: 'Scan'}} axis={chartAxis} />
-                  </Panel>
-                </Collapse>
-              </td>
-             </tr>
     });
     console.log(listItems);
 
